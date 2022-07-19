006e20528c487a783b027bac081a2ccd5abe911c
Shayan_Jahan <shayantec95@gmail.com>
2022-07-19 22:39:54 +0430

    Initialize Virtual Trading tab

--- CryptoApp/CryptoApp/ContentView.swift
+++ CryptoApp/CryptoApp/ContentView.swift
@@ -37,9 +37,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-            ContentView()
-        }
+        ContentView()
     }
 }
